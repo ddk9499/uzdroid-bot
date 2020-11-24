3cc6329d0aba782973a35503fd9aa98290825f50
Dostonbek Kamalov <ddk9499@gmail.com>
2020-11-24 10:52:09 +0500

    Add libraries and config sample.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,16 @@ group = "uz.dkamaloff"
 version = "0.0.1"
 
 repositories {
-    mavenCentral()
+    jcenter()
+    maven(url = "https://jitpack.io")
+    maven { url = uri("https://dl.bintray.com/heapy/heap-dev") }
+}
+
+dependencies {
+    val komodoVersion = "0.0.2-development-75"
+    implementation("io.heapy.komodo:komodo-config-dotenv:$komodoVersion")
+
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.0")
 }
 
 tasks.withType<KotlinCompile>() {
