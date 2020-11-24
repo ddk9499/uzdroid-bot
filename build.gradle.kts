7a65f0e276c15237665ad9016700b8181d23636c
Dostonbek Kamalov <ddk9499@gmail.com>
2020-11-24 11:07:32 +0500

    Update kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.20"
 }
 group = "uz.dkamaloff"
 version = "0.0.1"
