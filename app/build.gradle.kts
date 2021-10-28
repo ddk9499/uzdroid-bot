3f57596a99cc8cd321a8444b8f7dfe30d10e48f9
Adkhambek <mr.adkhambek@gmail.com>
2021-10-28 12:00:44 +0500

    add lib fatjar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("vertx-setup")
     id("kotlin-dagger-setup")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "uz.dkamaloff"
@@ -14,6 +17,26 @@ repositories {
 
 dependencies {
     implementation(Deps.Kotlin.coroutines)
-    implementation("io.netty:netty-resolver-dns-native-macos:4.1.59.Final:osx-x86_64")
     implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.0.4")
+}
+
+
+
+tasks.test {
+    useJUnit()
+}
+
+val shadowJar: ShadowJar by tasks
+shadowJar.apply {
+    manifest.attributes.apply {
+        put("Implementation-Version", "1.0.0")
+        put("Main-Class", "uz.dkamaloff.uzdroidbot.Application")
+    }
+
+    baseName = project.name + "-all"
+}
+tasks {
+    "build" {
+        dependsOn(shadowJar)
+    }
 }
